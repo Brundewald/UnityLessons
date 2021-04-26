63cdded941beeb8542070fad1b9f0d98ffb8b906
Brundewald <77693457+Brundewald@users.noreply.github.com>
2021-04-26 21:45:13 +0300

    Add jump and flooring.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,25 +8,40 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 5;
     [SerializeField] private Vector3 _direction;
     [SerializeField] private float _rotateSpeed = 50;
+    [SerializeField] private float _jump = 10;
+    [SerializeField] private Rigidbody rB;
+    
     // Start is called before the first frame update
     private void Start()
-    {
-        
+    {        
+       
     }
 
     // Update is called once per frame
     private void Update()
     {
-        _direction.x = Input.GetAxis("Horizontal");
-        _direction.z = Input.GetAxis("Vertical");
+        _direction.x = Input.GetAxis("Vertical");
+        _direction.z = Input.GetAxis("Horizontal");
+
+        rB = GetComponent<Rigidbody>();
+
+        if (rB.velocity.y == 0)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rB.AddForce(Vector3.up * _jump, ForceMode.Impulse);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        var speed = Vector3.forward * _direction.z * _speed * Time.deltaTime;
+        var speed = Vector3.right * _direction.x * _speed * Time.deltaTime;
         transform.Translate(speed);
 
-        var rotate = Vector3.up * _direction.x * _rotateSpeed * Time.deltaTime;
+        var rotate = Vector3.up * _direction.z * _rotateSpeed * Time.deltaTime;
         transform.Rotate(rotate);
+
+        
     }
 }
